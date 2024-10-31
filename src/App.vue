7cd9c9acc96e85ed5bfa5f3f28b5a17c10ef11e4
zuowendong <17326195691@163.com>
2024-10-31 21:06:18 +0800

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="w-full h-screen flex flex-col">
+    <header class="w-full h-14 flex items-center justify-center">FEI CUT EXAM</header>
+    <section class="w-full flex-1 grid grid-cols-[200px_auto_200px]">
+      <div>
+        <!-- components -->
+      </div>
+      <div class="bg-white"></div>
+      <div>
+        <!-- attrs -->
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
